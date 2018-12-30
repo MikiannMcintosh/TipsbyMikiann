a6b883abd8b1ccead7aacf544085d6d5db6d7ad8
Mikiann.Mcintosh <Mikiann.Mcintosh>
2018-12-30 15:21:10 -0500

    Initial Commit

---
+++ TipsbyMikiann/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipsbyMikiann
+//
+//  Created by Mikiann Mcintosh on 12/30/18.
+//  Copyright © 2018 Mikiann McIntosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
